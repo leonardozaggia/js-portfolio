e4478089e0dddca7176ce1778d0ce5c59d6bd3e9
leonardozaggia <118817367+leonardozaggia@users.noreply.github.com>
2023-08-12 10:33:04 +0000

    fix name

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/white_backgorund.jpg";
+import image from "../images/white_background.jpg.jpg";
 
 const imageAltText = "purple and blue abstract background";
 
