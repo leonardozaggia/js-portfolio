9195417b835114c139e9b066476686a547357828
leonardozaggia <118817367+leonardozaggia@users.noreply.github.com>
2023-08-12 10:16:10 +0000

    images changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/The_Sun_in_high_resolution.jpg";
+import image from "../images/tree.jpg";
 
 const imageAltText = "whatever you see in the image";
 
