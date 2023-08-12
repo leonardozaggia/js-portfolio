08427c23d3bcae41737e72956327b5c7121f15b4
leonardozaggia <118817367+leonardozaggia@users.noreply.github.com>
2023-08-12 10:31:49 +0000

    new pictures

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/deep_breath.jpg";
+import image from "../images/white_backgorund.jpg";
 
 const imageAltText = "purple and blue abstract background";
 
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/rose_on_dark.jpg";
+import image from "../images/portfolio_pic.jpg";
 
 const imageAltText = "tunnel with red lights";
 
